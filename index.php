778e7fade52fe34242c67860048970fdcc34d460
innosledoue <innocentngoran@gmail.com>
2019-07-11 19:51:11 +0000

    Ajout d'un fichier php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+ 
+    <body>
+
+	    <div id="corps">
+	        <h1>Mon super site</h1>
+	        
+	        <p>
+	            Bienvenue sur mon super site !<br />
+	            
+	        </p>
+	    </div>
+
+    </body>
+</html>
